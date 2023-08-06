6589af248ce70ef675bdaae38122d67f60157745
Neeraj Kashyap <krneeraj1997@gmail.com>
2023-08-06 09:48:49 +0530

    create note, update notelist, delete note functionality added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,16 +3,44 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import notes from "../notes";
+import CreateArea from "./CreateArea";
 
 
 function App() {
+    const [noteList,updateList]=React.useState([]);
+
+    function addNote(note){
+        console.log(note);
+        updateList((prevValue)=>{
+            return [...prevValue,note]
+        })
+    }
+    function deleteNote(id){
+        updateList((prevNotes)=>{
+            return prevNotes.filter((noteitem,index)=>{
+                return index!==id;
+            })
+        })
+    }
     return <div>
         <Header />
+        <CreateArea onAdd={addNote} />
         {notes.map( note =>
          <Note key={note.key} 
         title={note.title} 
         content={note.content} />
         )}
+        {noteList.map((noteItem,index)=>{
+            return (
+                <Note 
+                    key={index}
+                    id={index}
+                    title={noteItem.title}
+                    content={noteItem.content}
+                    dleteItem={deleteNote}
+                />
+            )
+        })}
         <Footer />
     </div>
 }
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -2,9 +2,13 @@ import React from "react";
 
 
 function Note(props) {
+    function handleDelete(){
+        props.dleteItem(props.id);
+    }
     return <div className="note">
         <h1>{props.title}</h1>
         <p>{props.content}</p>
+        <button onClick={handleDelete} >DELETE</button>
     </div>
 }
 
